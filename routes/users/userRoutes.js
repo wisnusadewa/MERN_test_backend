bdfe246eaccc1830b03e3c643d40eece102f5c60
wisnu sadewa <sadewwisnu@gmail.com>
2024-12-12 01:43:43 +0700

    perbaikan routes

--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const usersController = require('../../features/users/index');
+const middleware = require('../../config/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register', usersController.registerUser);
 router.post('/login', usersController.loginUser);
-router.get('/user/:id', usersController.getUserById);
+router.get('/user/:id', middleware.authenticateToken, usersController.getUserById);
 
 module.exports = router;
